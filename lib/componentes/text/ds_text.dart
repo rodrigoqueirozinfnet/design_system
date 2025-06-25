208c8a51117d95c8bcd3ba4f8f9613a9db0f0cc7
Rodrigo Alberto Queiroz Costa <rodrigo.costa@spassu.com.br>
2025-06-24 21:09:52 -0300

    ajustes

--- lib/componentes/text/ds_text.dart
+++ lib/componentes/text/ds_text.dart
@@ -2,12 +2,14 @@ import 'package:design_system/componentes/text/ds_text_style.dart';
 import 'package:flutter/cupertino.dart';
 
 class DsText extends StatelessWidget {
-  const DsText(this.text, {super.key, this.style});
+  const DsText(this.text, {super.key, this.style, this.overflow, this.maxLines});
   final String text;
-  final DsTextStyle ?style;
+  final TextOverflow? overflow;
+  final int? maxLines;
+  final DsTextStyle? style;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: style?.getStyle() ?? DsTextStyle.text.getStyle());
+    return Text(text, style: style?.getStyle() ?? DsTextStyle.text.getStyle(), maxLines: maxLines, overflow: overflow,);
   }
 }
